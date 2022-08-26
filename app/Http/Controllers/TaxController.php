13d1670484fc950faac6ad905f325296bb934ddf
Kestutis <kestutis.naureckas@insly.com>
2022-08-26 17:21:16 +0300

    ok

--- app/Http/Controllers/TaxController.php
+++ app/Http/Controllers/TaxController.php
@@ -22,11 +22,13 @@ class TaxController extends Controller
             'Alabama'=> [ 'tax' => 0.06, 'fee' => 0],
             'Alaska'=> [ 'tax' => 0.027, 'fee' => 0.01],
             'Arizona'=> [ 'tax' => 0.03, 'fee' => 0.002],
-            'Arkansas'=> [ 'tax' => 0.04, 'fee' => 0]
+            'Arkansas'=> [ 'tax' => 0.04, 'fee' => 0],
+            'Florida'=> [ 'tax' => 0.0494, 'fee' => 0.0002, 'empa' => 200]
         ];
 
         $surplus_tax_rate = data_get($taxes[$state],'tax',0);
         $stamping_fee_rate = data_get($taxes[$state],'fee',0);
+        $empa = data_get($taxes[$state],'empa',0);
        
         $surplus_tax = $tax_basis * $surplus_tax_rate;
         $stamping_fee = $tax_basis * $stamping_fee_rate;
@@ -42,6 +44,7 @@ class TaxController extends Controller
             'stamping_fee_rate' => $stamping_fee_rate,
             'surplus_tax' => $surplus_tax,
             'stamping_fee' => $stamping_fee,
+            'empa' => $empa,
             'total' => $total,
             'tax_basis' => $tax_basis,
             'type' => $type,
