040c44f637a05bea9bdd3ce44e12d37aceb16b27
Kestutis <kestutis.naureckas@insly.com>
2024-01-26 10:49:20 +0200

    yacht 3

--- app/Http/Controllers/YachtController.php
+++ app/Http/Controllers/YachtController.php
@@ -11,11 +11,46 @@ class YachtController extends Controller
  
         $result = [
             'status' => true,
-
+            'assured' => [
+                'name' => fake()->company(),
+                'address' => fake()->address(),
+            ],
+            'owners' => [
+                '1a3f457c-ae51-4e86-aee2-f1f95ff24ae4' =>
+                 [
+                    'name' => fake()->name(),
+                    'address' => fake()->address(),
+                    'nationality' => fake()->country(),
+                 ],
+                 '31669c25-2cf4-4849-be8e-0e215410f14d' =>
+                 [
+                    'name' => fake()->company(),
+                    'address' => fake()->address(),
+                    'nationality' => fake()->country(),
+                 ]
+            ],
+            'ownerExperience' => fake()->numberBetween(1, 20),
+            'cruisingRange' => fake()->numberBetween(100, 900) * 100,
+            'use' => fake()->randomElement([
+                "Cruising",
+                "Performance Cruising",
+                "Racing",
+                "Motor Yacht",
+                "Superyacht",
+                "Megayacht",
+                "Charter",
+                "Exploration",
+                "Fishing",
+                "Research",
+                "Sailing School",
+                "Day Sailing",
+                "Liveaboard",
+            ]),
             'yacht' => [
+              
                 'name' => $name,
                 'mooring' => fake()->city(),
-                'builder' => fake()->randomElements([
+                'builder' => fake()->randomElement([
                     "Lürssen",
                     "Feadship",
                     "Benetti",
@@ -34,6 +69,77 @@ class YachtController extends Controller
                     'beam' => fake()->randomFloat(1, 4, 9),
                     'draft' => fake()->randomFloat(1, 1, 3),
                 ],
+                'mastMaterial' => fake()->randomElement([
+                    "Aluminum",
+                    "Carbon Fiber",
+                    "Wood",
+                    "Composite",
+                    "Steel",
+                    "Hybrid",
+                    "Fiberglass"
+                ]),
+                'speed' => fake()->numberBetween(7, 35),
+                'guests' => fake()->numberBetween(4, 40),
+                'imo' => fake()->numberBetween(2000000, 9000000),
+                'flag' => fake()->country(),
+                'type' => fake()->randomElement([
+                    "Motor Yacht", 
+                    "Sailing Yacht", 
+                ]),
+                'tonage' => fake()->numberBetween(100, 200),
+                'propulsion' => fake()->randomElement([
+                    "Sail", 
+                    "Internal Combustion Engines",
+                    "Hybrid Propulsion",
+                    "Electric Propulsion"
+                ]),
+                'helicopter' => fake()->boolean(),
+                'callSign' => fake()->randomElement([
+                    "ABC1234",
+                    "XYZ5678",
+                    "DEF9012",
+                    "GHI3456",
+                    "JKL7890",
+                ]),
+                'hullNo' => fake()->randomElement([
+                    "US-ABC12345G819",
+                    "FR-XYZ98765H723",
+                    "CA-DEF45678K625",
+                    "UK-GHI65432R520",
+                    "AU-JKL78901M317",
+                ]),
+
+             ],
+             'engine' => [
+                'make' => fake()->randomElement([
+                    "Caterpillar",
+                    "MTU",
+                    "MAN",
+                    "Volvo Penta",
+                    "Cummins",
+                    "Yanmar",
+                    "Mercury Marine",
+                    "Suzuki Marine",
+                    "Honda Marine",
+                    "Perkins",
+                    "Nanni Diesel",
+                    "John Deere",
+                ]),
+                'hp' =>  fake()->numberBetween(100, 5000),
+                'serial' => fake()->randomElement([
+                    "CAT123456",
+                    "MTU987654",
+                    "VOLVO567890",
+                    "CUMMINS456789",
+                    "YANMAR123456",
+                ]),
+                'survey' =>  fake()->dateTimeBetween('-3 years', '-1 years'),
+                'year' =>  fake()->numberBetween(1990, 2024),
+                'fuel' => fake()->randomElement([
+                    "Petrol",
+                    "Diesel" 
+                ]),
+                'refit' =>  fake()->numberBetween(100, 5000),
              ],
              'tenders' => [
                 'name' => $name,
