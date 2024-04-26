282c6d36749a7cb16ba6920d4aaee142021ce95a
Kestutis <kestutis.naureckas@insly.com>
2024-04-26 11:00:39 +0300

    push

--- app/Http/Controllers/AIModelController.php
+++ app/Http/Controllers/AIModelController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
 
 class AIModelController extends Controller
 {
@@ -13,11 +14,32 @@ class AIModelController extends Controller
     {
 
         $response = [
-            'result' => 'success',
+            'result' => 'Risk of you getting into an accident is 10.5%.',
             'data' => request()->all() 
         ];
+       // $this->storeRequestData($response);
+
+        //store request data parameters to json file on disk
+        
+        
    
         return $response;
+    }
+
+    private function storeRequestData($data)
+    {
+      
+        
+        // get current date and time as string
+        $now = date('Y-m-d H:i:s ');
+
+
+        $filename = $now.Str::random(40) . '.json';
+
+
+        Storage::disk('public')->put($filename, json_encode($data));
+      
+
     }
  
 }
